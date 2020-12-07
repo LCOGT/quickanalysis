ca33ce9a23ce64b7ecfe3596c3c6644b83331910
timbeccue <timothybeccue@gmail.com>
2020-12-07 15:10:46 -0800

    Travis bug

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='quickanalysis',
-    packages=['quickanalysis'],
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'flask',
