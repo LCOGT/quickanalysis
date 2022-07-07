b70f81b0695687c9a1f37d980b60bdba25d166e9
Tim Beccue <timothybeccue@gmail.com>
2022-07-07 12:54:34 -0700

    Fix tests for updated method

--- tests/unit/test_load_data.py
+++ tests/unit/test_load_data.py
@@ -12,23 +12,28 @@ VALID_TEXT_FILENAME = "tst-test-20201112-00000058-EX00.txt"
 
 
 def test_check_if_s3_image_exists_good_filename():
-    assert check_if_s3_image_exists(VALID_FITS_FILENAME)
+    s3_directory = "data"
+    assert check_if_s3_image_exists(VALID_FITS_FILENAME, s3_directory)
 
 
 def test_check_if_s3_image_exists_bad_filename():
-    assert not check_if_s3_image_exists(NONEXISTANT_FILE)
+    s3_directory = "data"
+    assert not check_if_s3_image_exists(NONEXISTANT_FILE, s3_directory)
 
 
 def test_get_image_data():
-    data = get_image_data(VALID_FITS_FILENAME)
+    s3_directory = "data"
+    data = get_image_data(VALID_FITS_FILENAME, s3_directory)
     assert isinstance(data, np.ndarray)
 
 
 def test_get_image_data_invalid_filename():
+    s3_directory = "data"
     with pytest.raises(Exception):
-        data = get_image_data(NONEXISTANT_FILE)
+        data = get_image_data(NONEXISTANT_FILE, s3_directory)
 
 
 def test_get_image_data_text_file():
+    s3_directory = "data"
     with pytest.raises(Exception):
-        data = get_image_data(VALID_TEXT_FILENAME)
+        data = get_image_data(VALID_TEXT_FILENAME, s3_directory)
