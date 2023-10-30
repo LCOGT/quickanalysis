ca48de0724316c7530a5581fd6a8e85ddfc4cd87
Jon <jnation@lco.global>
2023-10-30 22:13:29 -0100

    Fixed tests, but really tests shouldn't require s3 connection

--- tests/unit/test_load_data.py
+++ tests/unit/test_load_data.py
@@ -7,33 +7,26 @@ from quickanalysis.utils.load_data import check_if_s3_image_exists
 from quickanalysis.utils.load_data import get_image_data
 
 NONEXISTENT_FILE = "im a string not a filename"
-VALID_FITS_FILENAME = "tst-test-20201112-00000058-EX10.fits.bz2"
-VALID_TEXT_FILENAME = "tst-test-20201112-00000058-EX00.txt"
+VALID_FITS_FILENAME = "eco2-ec002cs-20230828-00021260B1-EX00.fits.fz"
 
 
 def test_check_if_s3_image_exists_good_filename():
-    s3_directory = "data"
+    s3_directory = "eco/ec002cs/20230828/raw"
     assert check_if_s3_image_exists(VALID_FITS_FILENAME, s3_directory)
 
 
 def test_check_if_s3_image_exists_bad_filename():
-    s3_directory = "data"
+    s3_directory = "eco/ec002cs/20230828/raw"
     assert not check_if_s3_image_exists(NONEXISTENT_FILE, s3_directory)
 
 
 def test_get_image_data():
-    s3_directory = "data"
+    s3_directory = "eco/ec002cs/20230828/raw"
     data = get_image_data(VALID_FITS_FILENAME, s3_directory)
     assert isinstance(data, np.ndarray)
 
 
 def test_get_image_data_invalid_filename():
-    s3_directory = "data"
+    s3_directory = "eco/ec002cs/20230828/raw"
     with pytest.raises(Exception):
         data = get_image_data(NONEXISTENT_FILE, s3_directory)
-
-
-def test_get_image_data_text_file():
-    s3_directory = "data"
-    with pytest.raises(Exception):
-        data = get_image_data(VALID_TEXT_FILENAME, s3_directory)
